540a08a58edae65bdeb3a41c65bde30f914e02fa
marko renic <marko.renic@example.com>
2020-05-06 13:54:09 -0500

    show/hide comments +style

--- public_html/functions.php
+++ public_html/functions.php
@@ -1,91 +1,181 @@
-<?php
-error_reporting(E_ALL);
-#connection info
-$dbname = 'dbwmbsxra9qfuj';
-$dbuser = 'um7bdcyp85wnp';
-$dbpass = '351e)|1@$)u4';
-$dbhost = 'localhost';
-///creates a connection
-$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-function GetLogin($connect){
-	// cheks if LoginSubmit is pressed 
-	if (isset($_POST['loginSubmit'])) {
-		$uid = $_POST['uid'];
-		$pwd = $_POST['pwd'];
-		$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
-		$result = $connect->query($sql);
-		if (mysqli_num_rows($result) > 0){
-			if ($row = $result->fetch_assoc()) {
-				//sets session uid to username 
-              	$_SESSION['uid'] = $uid;
-              	//refreshes the page fil
-				echo "<meta http-equiv='refresh' content='0;url=https://ibdocs.renic.xyz/index.php'>";
-				exit();
-			}
-		}
-		else{ echo("<p class='font1'>password or username invalid<p>");}
-	}
-}
-function userLogout() {
-	if (isset($_POST['LogoutSubmit'])) {
-      	session_destroy();
-      	echo("<meta http-equiv='refresh' content='1'>");
-		exit();
-      	}
-}
-
-function signup($connect) {
-	if (isset($_POST['singupSubmit'])) {
-		$username = $_POST['uid'];
-		$password = $_POST['pwd'];
-		$ip = $_SERVER['REMOTE_ADDR'];
-		//checks if the password has all the requirments 
-		//$password_exp ="/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z]{8,12}$/";
-		$query = mysqli_query($connect,"SELECT uid FROM user  WHERE uid='".$username."'");
-	    if (mysqli_num_rows($query) == 0){
-		$sql = "INSERT INTO user (uid, pwd) VALUES ('$username', '$password')";
-	    $result = $connect->query($sql);
-		$sql = "INSERT INTO ip (uid, ip) VALUES ('$username', '$ip')";
-	    $result = $connect->query($sql);
-		header("Location: login.php?signupsuccess");
-		}else{
-	       echo "username already taken";
-	    }
-      	
-	}
-}
-function Report($connect){
-    $qn= $_SESSION["qn"];
-	$sql = "SELECT rn FROM report WHERE qn='$qn'";
-	$result = $connect->query($sql);
-    if (mysqli_num_rows($result) > 0){
-       $sql = "UPDATE report SET rn = rn + 1 WHERE qn ='$qn'";
-      	$result = $connect->query($sql);
-    } else{
-        
-		$sql = "INSERT INTO report (qn,rn) VALUES ('$qn', 1)";
-	    $result = $connect->query($sql);
-    }
-}
-function comment($connect){
-    if (isset($_POST['commentbutton'])) {
-        $section = $_POST['section'];
-        $date = $_POST['date'];
-        $uid = $_SESSION['uid'];
-        $text = $_POST['text'];
-        $sql = "INSERT INTO comments (uid, date, text, section) VALUES ('$uid', '$date', '$text','$section')";
-		$result = $connect->query($sql);
-}}
-function displaycomments($connect){
-    $section = $_SESSION["qn"];
-    $sql = "SELECT * FROM comments WHERE section='$section' ORDER BY date DESC";
-	$result = $connect->query($sql);
-	while ($row = $result->fetch_assoc()) {
-	echo "<div class='comment box'>";
-		echo $row['uid']."<br>";
-		echo $row['date']."<br>";
-		echo $row['text'];
-	echo "</div>";
-	}
+<?php
+
+error_reporting(E_ALL);
+
+#connection info
+
+$dbname = 'dbwmbsxra9qfuj';
+
+$dbuser = 'um7bdcyp85wnp';
+
+$dbpass = '351e)|1@$)u4';
+
+$dbhost = 'localhost';
+
+///creates a connection
+
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+
+function GetLogin($connect){
+
+	// cheks if LoginSubmit is pressed 
+
+	if (isset($_POST['loginSubmit'])) {
+
+		$uid = $_POST['uid'];
+
+		$pwd = $_POST['pwd'];
+
+		$sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
+
+		$result = $connect->query($sql);
+
+		if (mysqli_num_rows($result) > 0){
+
+			if ($row = $result->fetch_assoc()) {
+
+				//sets session uid to username 
+
+              	$_SESSION['uid'] = $uid;
+
+              	//refreshes the page fil
+
+				echo "<meta http-equiv='refresh' content='0;url=https://ibdocs.renic.xyz/index.php'>";
+
+				exit();
+
+			}
+
+		}
+
+		else{ echo("<p class='font1'>password or username invalid<p>");}
+
+	}
+
+}
+
+function userLogout() {
+
+	if (isset($_POST['LogoutSubmit'])) {
+
+      	session_destroy();
+
+      	echo("<meta http-equiv='refresh' content='1'>");
+
+		exit();
+
+      	}
+
+}
+
+
+
+function signup($connect) {
+
+	if (isset($_POST['singupSubmit'])) {
+
+		$username = $_POST['uid'];
+
+		$password = $_POST['pwd'];
+
+		$ip = $_SERVER['REMOTE_ADDR'];
+
+		//checks if the password has all the requirments 
+
+		//$password_exp ="/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z]{8,12}$/";
+
+		$query = mysqli_query($connect,"SELECT uid FROM user  WHERE uid='".$username."'");
+
+	    if (mysqli_num_rows($query) == 0){
+
+		$sql = "INSERT INTO user (uid, pwd) VALUES ('$username', '$password')";
+
+	    $result = $connect->query($sql);
+
+		$sql = "INSERT INTO ip (uid, ip) VALUES ('$username', '$ip')";
+
+	    $result = $connect->query($sql);
+
+		header("Location: login.php?signupsuccess");
+
+		}else{
+
+	       echo "username already taken";
+
+	    }
+
+      	
+
+	}
+
+}
+
+function Report($connect){
+
+    $qn= $_SESSION["qn"];
+
+	$sql = "SELECT rn FROM report WHERE qn='$qn'";
+
+	$result = $connect->query($sql);
+
+    if (mysqli_num_rows($result) > 0){
+
+       $sql = "UPDATE report SET rn = rn + 1 WHERE qn ='$qn'";
+
+      	$result = $connect->query($sql);
+
+    } else{
+
+        
+
+		$sql = "INSERT INTO report (qn,rn) VALUES ('$qn', 1)";
+
+	    $result = $connect->query($sql);
+
+    }
+
+}
+
+function comment($connect){
+
+    if (isset($_POST['commentbutton'])) {
+
+        $section = $_POST['section'];
+
+        $date = $_POST['date'];
+
+        $uid = $_SESSION['uid'];
+
+        $text = $_POST['text'];
+
+        $sql = "INSERT INTO comments (uid, date, text, section) VALUES ('$uid', '$date', '$text','$section')";
+
+		$result = $connect->query($sql);
+
+}}
+
+function displaycomments($connect){
+
+    $section = $_SESSION["qn"];
+
+    $sql = "SELECT * FROM comments WHERE section='$section' ORDER BY date DESC";
+
+	$result = $connect->query($sql);
+
+	while ($row = $result->fetch_assoc()) {
+
+	echo "<div class='comment box comments'>";
+
+		echo $row['uid']."<br>";
+
+		echo $row['date']."<br>";
+
+		echo $row['text'];
+
+	echo "</div>";
+
+	}
+
 }
--- public_html/physics.php
+++ public_html/physics.php
@@ -1,119 +1,229 @@
-<?php 
-session_start();
-include "functions.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style/subject.css">
-    <title>Physics</title>
-</head>
-<?php include "misc/header.php";?>
-
-<script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-
-<body>
-    <div class="grid-container">
-        <div class="grid-item question">
-        <!-- Get Random Question -->
-        Question Reference:
-        <?php 
-        $dir  = 'physics/'.$_GET["topic"];
-        $questions = scandir($dir);
-        $len = count ($questions);
-        #print_r($questions);
-        $question_name = $questions[random_int(2,$len)];
-        $_SESSION["qn"] = $question_name;
-        #print_r($question_name);
-        $question = ("physics/".$_GET["topic"]."/".$question_name);
-        include $question?>
-        </div>
-
-        <div class="grid-item">Click the corrent answer:
-            <ul>
-                <li class="popup"><button onclick="clickHandler('A')" class="A">A</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
-                <li class="popup"><button onclick="clickHandler('B')" class="B">B</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
-                <li class="popup"><button onclick="clickHandler('C')" class="C">C</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
-                <li class="popup"><button onclick="clickHandler('D')" class="D">D</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
-            </ul>
-
-<?php
-            echo "<form method='POST' action='".Report($connect)."'>
-				<button type='submit' name='ReportSubmit'>Report</button>
-                </form>";
-?>
-            
-        </div>
-        <div class="grid-item">
-        <?php
-        echo "<form method='POST' action'".Comment($connect)."'>
-          <input type='hidden' name='section' value= '".$_SESSION["qn"]."'>
-          <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
-          <textarea name='text'></textarea><br><br>
-          <button type='submit' name='commentbutton'>Comment</button>
-        </form>";
-
-        displaycomments($connect);
-        ?>
-        </div>
-        <div class="grid-item mobile mdd"><a>Topic: <?php print_r($dir);?><br>
-        Change Topic:</a>
-                        <li><a href="physics.php?topic=Core">All</a></li>
-                        <li><a href="physics.php?topic=Topic1">Topic 1</a></li>
-                        <li><a href="physics.php?topic=Topic2">Topic 2</a></li>
-                        <li><a href="physics.php?topic=Topic3">Topic 3</a></li>
-                        <li><a href="physics.php?topic=Topic4">Topic 4</a></li>
-                        <li><a href="physics.php?topic=Topic5">Topic 5</a></li>
-                        <li><a href="physics.php?topic=Topic6">Topic 6</a></li>
-                        <li><a href="physics.php?topic=Topic7">Topic 7</a></li>
-                        <li><a href="physics.php?topic=Topic8">Topic 8</a></li>
-                        <li><a href="physics.php?topic=Topic9">Topic 9</a></li>
-                        <li><a href="physics.php?topic=Topic10">Topic 10</a></li>
-                        <li><a href="physics.php?topic=Topic11">Topic 11</a></li>
-                        <li><a href="physics.php?topic=Topic12">Topic 12</a></li>
-        </div>
-    </div>
-
-</body>
-
-
-<script>
-    try{
-    var answer = document.getElementById("answer");
-    var letter_answer = answer.getAttribute("value");
-    }
-    catch(TypeError){
-        location.href="physics.php?topic=Core";
-    }
-    
-    function toggleshow(element){
-        element.classList.toggle("show")
-    }
-    function clickHandler(letter) {
-    if (letter == letter_answer){
-        console.log("Correct answer");
-        popupcorrect();
-        setTimeout(function() {location.reload();},1000);
-    }
-    else{
-        popupwrong();
-        setTimeout(2000);
-    }
-    }
-
-    // When the user clicks on <div>, open the popup
-    function popupcorrect() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-    }
-    function popupwrong() {
-    var popup = document.getElementById("WmyPopup");
-    popup.classList.toggle("show");
-    setTimeout(function(){toggleshow(popup);},2000);
-    }
-</script>
-
+<?php 
+
+session_start();
+
+include "functions.php";
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style/subject.css">
+
+    <title>Physics</title>
+
+</head>
+
+<?php include "misc/header.php";?>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+
+
+
+<body class="body">
+
+    <div class="grid-container">
+
+        <div class="grid-item question">
+
+        <!-- Get Random Question -->
+
+        Question Reference:
+
+        <?php 
+
+        $dir  = 'physics/'.$_GET["topic"];
+
+        $questions = scandir($dir);
+
+        $len = count ($questions);
+
+        #print_r($questions);
+
+        $question_name = $questions[random_int(2,$len)];
+
+        $_SESSION["qn"] = $question_name;
+
+        #print_r($question_name);
+
+        $question = ("physics/".$_GET["topic"]."/".$question_name);
+
+        include $question?>
+
+        </div>
+
+
+
+        <div class="grid-item answer">Click the corrent answer:
+
+            <ul>
+
+                <li class="popup"><button onclick="clickHandler('A')" class="A">A</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
+
+                <li class="popup"><button onclick="clickHandler('B')" class="B">B</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
+
+                <li class="popup"><button onclick="clickHandler('C')" class="C">C</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
+
+                <li class="popup"><button onclick="clickHandler('D')" class="D">D</button><span class="popuptext" id="myPopup">Correct!</span><span class="Wpopuptext" id="WmyPopup">Wrong, try again!</span></li>
+
+            </ul>
+
+
+
+<?php
+
+            echo "<form method='POST' action='".Report($connect)."'>
+
+				<button type='submit' name='ReportSubmit'>Report Question</button></br>
+
+                </form>";
+
+?>
+
+            
+
+        </div>
+
+        <div class="grid-item comment-section">
+        <button onclick="toggleComments()">Toggle comments</button>
+        <div class="comments-container" id="comments-container">
+
+        <?php
+
+        echo "<form method='POST' action'".Comment($connect)."'>
+
+          <input type='hidden' name='section' value= '".$_SESSION["qn"]."'>
+
+          <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
+
+          <textarea class='comment-box' name='text'></textarea><br><br>
+
+          <button type='submit' name='commentbutton'>Comment</button>
+
+        </form>";?>
+
+        <?php displaycomments($connect); ?>
+        </div>
+        </div>
+
+        <div class="grid-item mobile mdd"><a>Topic: <?php print_r($dir);?><br>
+        Change Topic:</a>
+                        <li><a href="physics.php?topic=Core">All</a></li>
+                        <li><a href="physics.php?topic=Topic1">Topic 1</a></li>
+                        <li><a href="physics.php?topic=Topic2">Topic 2</a></li>
+                        <li><a href="physics.php?topic=Topic3">Topic 3</a></li>
+                        <li><a href="physics.php?topic=Topic4">Topic 4</a></li>
+                        <li><a href="physics.php?topic=Topic5">Topic 5</a></li>
+                        <li><a href="physics.php?topic=Topic6">Topic 6</a></li>
+                        <li><a href="physics.php?topic=Topic7">Topic 7</a></li>
+                        <li><a href="physics.php?topic=Topic8">Topic 8</a></li>
+                        <li><a href="physics.php?topic=Topic9">Topic 9</a></li>
+                        <li><a href="physics.php?topic=Topic10">Topic 10</a></li>
+                        <li><a href="physics.php?topic=Topic11">Topic 11</a></li>
+                        <li><a href="physics.php?topic=Topic12">Topic 12</a></li>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+
+
+
+
+<script>
+
+    try{
+
+    var answer = document.getElementById("answer");
+
+    var letter_answer = answer.getAttribute("value");
+
+    }
+
+    catch(TypeError){
+
+        location.href="physics.php?topic=Core";
+
+    }
+
+    
+
+    function toggleshow(element){
+
+        element.classList.toggle("show")
+
+    }
+
+    function clickHandler(letter) {
+
+    if (letter == letter_answer){
+
+        console.log("Correct answer");
+
+        popupcorrect();
+
+        setTimeout(function() {location.reload();},1000);
+
+    }
+
+    else{
+
+        popupwrong();
+
+        setTimeout(2000);
+
+    }
+
+    }
+
+
+
+    // When the user clicks on <div>, open the popup
+
+    function popupcorrect() {
+
+    var popup = document.getElementById("myPopup");
+
+    popup.classList.toggle("show");
+
+    }
+
+    function popupwrong() {
+
+    var popup = document.getElementById("WmyPopup");
+
+    popup.classList.toggle("show");
+
+    setTimeout(function(){toggleshow(popup);},2000);
+
+    }
+
+    function toggleComments() {
+    var x = document.getElementById("comments-container");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+
+</script>
+
+
+
 </html>
