11f5645f67427a07153b54740415edd892653caf
marko renic <marko.renic@example.com>
2020-05-06 19:14:05 -0500

    Added homepage

--- public_html/index.php
+++ public_html/index.php
@@ -1,18 +1,74 @@
-<?php
-session_start();
-
-?>
-<html>
-<head>
-<link rel="shortcut icon" type="image/png" href="resources/logo.png">
-    <link rel="stylesheet" href="style/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-<div><?php include "misc/header.php" ?></div>
-
-<body>
-    <div class="grid-container">
-        <div class="grid">Homepage is Work in progress...<br> To practice questions go to Physics-> Topic </div>
-    </div>
-</body>
+<?php
+
+session_start();
+
+?>
+
+<html>
+
+<head>
+
+<link rel="shortcut icon" type="image/png" href="resources/logo.png">
+
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/homepage/homepage.css">
+    <link rel="stylesheet" href="style/homepage/animate.css">
+    <link rel="stylesheet" href="style/homepage/fontawesome.css">
+    <link rel="stylesheet" href="style/homepage/main.css">
+    <link rel="stylesheet" href="style/homepage/solid.css">
+    <link rel="stylesheet" href="style/homepage/normalize.css">
+    <link rel="stylesheet" href="style/homepage/bootstrap.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+</head>
+
+<div><?php include "misc/header.php" ?></div>
+
+
+
+<body>
+
+    <div class="grid-container">
+
+        <div class="grid">Homepage is Work in progress...<br> To practice questions go to Physics-> Topic 
+        <div class="row services">
+                
+                <a href="questionbank.ibo.org/en/teachers/00000/questionbanks/43-dp-biology/syllabus_sections.html" target="_blank">
+                <div class="col-md-4">
+                    <div class="service">
+                      <h4 class="heading">Biology</h4>
+                      <p class="description">
+                        <br>        
+                        <img src="resources/ib-qb-43-logo-cdfae0cc01fed52ce940ddc496811bda61a76e8309cc51fa8c7ab73ed9dc8acb.jpg" alt="IBQBLogo" style="width:275px;height:58px;">  
+                      </p>
+                    </div>
+                </div>
+                </a>
+                <a href="http://ibdocs.renic.xyz/chemistry.php?topic=Core" target="_blank">
+                <div class="col-md-4">
+                    <div class="service">
+                      <h4 class="heading">Chemistry</h4>
+                      <p class="description">
+                        <br>
+                            <img src="resources/ib-qb-45-logo-76d69de0db944ddd9e10134eb5bfca316735f279c439a3b51cea24f5bdab47d2.jpg" alt="IBQBLogo" style="width:275px;height:57px;">
+                      </p>
+                    </div>
+                </div>
+                </a>
+                <a href="http://ibdocs.renic.xyz/physics.php?topic=Core" target="_blank">
+                <div class="col-md-4">
+                    <div class="service">
+                      <h4 class="heading">Physics</h4>
+                      <p class="description">
+                        <br>        
+                            <img src="resources/ib-qb-46-logo-683ef0176708d789b2acbf6ece48c55de4cd5ddac781fb455afe3540d22d050e.jpg" alt="IBQBLogo" style="width:275px;height:59px;">
+                      </p>
+                    </div>
+                </div>
+                </a>
+    </div>
+
+</body>
+
 </html>
